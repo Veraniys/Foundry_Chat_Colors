9b9147a29e254701aa89eecb697b7450dc2ea7ba
Weronika <69217021+Veraniys@users.noreply.github.com>
2026-06-02 10:30:55 +0200

    Add files via upload

---
+++ module.js
@@ -0,0 +1,46 @@
+Hooks.once("init", async () => {
+  console.log("Chat Colors | Loading...")
+});
+
+Hooks.on("renderChatMessage", async (message, html, speakerInfo) => {
+    ChatPortrait.onRenderChatMessage(message, html, speakerInfo);
+})
+
+const _ChatPortrait = class _ChatPortrait {
+    static onRenderChatMessage(chatMessage, html, speakerInfo) {
+        let authorColor = "black";
+        if (speakerInfo.author) {
+            authorColor = speakerInfo.author.color;
+        } else {
+            authorColor = speakerInfo?.user.color;
+        }
+        const messageData = speakerInfo.message;
+        _ChatPortrait.setChatMessageBorder(html, messageData, authorColor);
+    }
+
+    // static onRenderChatMessageInternal(chatMessage, html, speakerInfo, messageSender, messageHeaderBase, elementItemImageList, elementItemNameList, elementItemContentList, elementItemTextList, imageReplacer, gameSystemId, doNotPrintPortrait, doOnlyPortrait) {
+    //     const messageDataBase = speakerInfo;
+    //     let authorColor = "black";
+    //     if (messageDataBase.author) {
+    //     authorColor = messageDataBase.author.color;
+    //     } else {
+    //     authorColor = messageDataBase?.user.color;
+    //     }
+    //     const messageData = messageDataBase.message;
+    //     _ChatPortrait.setChatMessageBorder(html, messageData, authorColor);
+    // }
+
+    static setChatMessageBorder(html, messageData, authorColor) {
+        let authorColor = "black";
+        if (speakerInfo.author) {
+            authorColor = speakerInfo.author.color;
+        } else {
+            authorColor = speakerInfo?.user.color;
+        }
+
+        if (!messageData.borderColor) {
+            html[0].style.borderColor = authorColor;
+            messageData.borderColor = authorColor;
+        }
+    }
+}
